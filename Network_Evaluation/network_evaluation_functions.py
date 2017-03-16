eb4ad67d3fa9dad2072e8484cabee91cd559a67d
Justin Huang <justin.k.huang@gmail.com>
2017-03-16 15:28:44 -0400

    Finished first draft of network evaluation script. Speed and performance needs to be tested

--- Network_Evaluation/network_evaluation_functions.py
+++ Network_Evaluation/network_evaluation_functions.py
@@ -1,9 +1,7 @@
-import networkx as nx
-import numpy as np
-
 ##################################################
 # ---------- General Helper Functions ---------- #
 ##################################################
+import networkx as nx
 
 # Load network from file
 def load_network_file(network_file_path, delimiter):
@@ -13,6 +11,9 @@ def load_network_file(network_file_path, delimiter):
 #######################################################
 # ---------- Network Propagation Functions ---------- #
 #######################################################
+import time
+import numpy as np
+import scipy
 
 # Normalize network (or network subgraph) for random walk propagation
 def normalize_network(network):
@@ -38,15 +39,35 @@ def calculate_alpha(network):
 	return alpha_val
 
 # Propagate binary matrix (each row is different node set to propagate)
-def network_propagation(network, binary_node_sets_matrix, alpha, method='power'):
+def network_propagation(network, binary_node_sets_matrix, method='power'):
+	# Calculate alpha from network
+	network_alpha = calculate_alpha(network)
+
+	# Closed form random-walk propagation (as seen in HotNet2)
+	if method=='closed':
+		return prop_data
+	# Iterative form random-walk propagation (as seen in NBS)
+	else:
+		return prop_data
+
 
 ###########################################################################
 # ---------- Patient Similarity Network Construction Functions ---------- #
 ###########################################################################
+import pandas as pd
+from sklearn.decomposition import PCA
 
 # Load somatic mutation data (construct binary somatic mutation matrix in context of molecular network)
-def load_sm_data(sm_data_file):
-
+def load_TCGA_MAF(MAF_file):
+	# Process MAF file to produce dictionary of patient mutations
+    TCGA_MAF = pd.read_csv(maf_file,sep='\t',low_memory=False)
+    # Convert to matrix format
+    TCGA_sm_mat = TCGA_MAF.groupby(['Tumor_Sample_Barcode', 'Hugo_Symbol']).size().unstack().fillna(0)
+    # Save only as int
+    TCGA_sm_mat = (TCGA_sm_mat>0).astype(int)
+    # Trim TCGA barcodes
+    TCGA_sm_mat.index = [pat[:12] for pat in TCGA_sm_mat.index]
+    return TCGA_sm_mat
 
 # Mean-centering of propagated somatic mutation profiles
 def mean_center_data(propagated_sm_matrix):
@@ -118,11 +139,12 @@ def PSN_construction(similarity_df, k):
 	print 'Network construction complete:', time.time()-starttime, 'seconds'
 	return G
 
-# PSN Construction wrapper
-
 ################################################################
 # ---------- Set-Based Network Evaluation Functions ---------- #
 ################################################################
+import random
+import scipy.stats as stats
+import sklearn.metrics as metrics
 
 # Shuffle network in degree-preserving manner
 def shuffle_network(network):
@@ -136,17 +158,91 @@ def shuffle_network(network):
 	print 'Network shuffled:', time.time()-shuff_time, 'seconds. Edge similarity:', shared_edges/float(len(network.edges()))
 	return shuff_net
 
-# Load node sets for SBNE (construct binary matrix for propagation in context of network for SBNE)
-def load_SBNE_node_sets(node_set_file, network):
+# Construct node-set sub-sampled binary matrix for propagation in context of network for SBNE with given sub-sample proportion p, and sub-sample number of iterations n
+def SBNE_binary_matrix_constructor(node_set_file, network, p, n):
 	# Construct node set sub-sample matrices
+	f = open(node_set_file)
+	node_set_lines = f.read().splitlines()
+	node_set_lines_split = [line.split('\t') for line in node_set_lines]
+	f.close()
+	node_sets = {node_set[0]:set(node_set[1:]) for node_set in node_set_lines_split}
+	node_set_names = sorted(node_sets.keys())
+	# Initialize dataframe for node set sub-samples to be propagated
+	network_nodes = set(network.nodes())
+	node_set_sub_sample_binary_matrix_index = []
+	for node_set in node_set_names:
+		node_set_index = [node_set+'_'+repr(i) for i in range(1,n+1)]
+		node_set_sub_sample_binary_matrix_index = node_set_sub_sample_binary_matrix_index + node_set_index
+	node_set_sub_sample_binary_matrix = pd.DataFrame(0, index = node_set_sub_sample_binary_matrix_index, columns = list(network_nodes))
+	# Sub-sample the intersection of each node set with the network node set
+	for node_set in node_set_names:
+		intersect = list(node_sets[node_set].intersection(network_nodes))
+		sample_size = int(round(p*len(intersect)))
+		for i in range(1, n+1):
+			node_set_sub_sample = random.sample(intersect, sample_size) for i in range(n)
+			node_set_sub_sample_binary_matrix.ix[node_set+'_'+repr(i)][node_set_sub_sample] = 1
+	return node_sets, node_set_sub_sample_binary_matrix
 
-# AUPRC Analysis for each node set
+# AUPRC Analysis for each node set using labelled propagated node set sub-sample dataframe 
+def AUPRC_Analysis(node_set_sub_sample_binary_matrix, node_sets):
+	runtime = time.time()
+	# Propagate sub-sampled node sets
+	prop_sub_samples = network_propagation(network, node_set_sub_sample_binary_matrix)
+	# AUPRC Analysis
+	node_set_names = sorted(node_sets.keys())
+	n_sub_samples = prop_sub_samples.shape[0] / len(node_set_names)
+	node_set_AUPRC_results = []
+	for node_set in node_set_names:
+		node_set_AUPRCs = []
+		for i in range(1, n_sub_samples+1):
+			node_set_index = node_set+'_'+repr(i)
+			# Get sub-sampled nodes
+			sample = set(node_set_sub_sample_binary_matrix.ix[node_set_index][node_set_sub_sample_binary_matrix.ix[node_set_index]==1].index)
+			# Get remaining nodes in node set not sub-sampled
+			node_set_non_sample = node_sets[node_set].difference(sample)
+			# Sort all non-sampled_genes by propagation score
+			sorted_nodes = prop_sub_samples.ix[node_set_index][prop_sub_samples.columns[~prop_sub_samples.columns.isin(sample)]].sort(ascending=False, inplace=False).index
+			# Binary vector of sorted genes by propagation score marking non-sampled node set nodes
+			y_actual = [1 if node in node_set_non_sample else 0 for node in sorted_nodes]
+			# Identify points on the node list to calculate precision/recall
+			sorted_node_set_non_sample_index = [j+1 for j in range(len(y_actual)) if y_actual[j]==1]
+			# Construct precision-recall curve and calculate AUPRC
+			precision, recall = [], []
+			for k in sorted_node_set_non_sample_index:
+			    TP, FN =  sum(y_actual[:k]), sum(y_actual[k:])
+			    precision.append(TP/float(k))
+			    recall.append(TP/float(TP+FN))
+			precision = [1]+precision+[0]
+			recall=[0]+recall+[1]
+			node_set_AUPRCs.append(metrics.auc(recall, precision))
+		# Average and Variance of AUPRCs for across all sub-samplings of node set
+		node_set_AUPRC_results = [node_set, np.mean(node_set_AUPRCs), np.var(node_set_AUPRCs)]
+	return pd.DataFrame(node_set_AUPRC_results, columns = ['Node Set Name', 'Avg AUPRC', 'AUPRC Var']).set_index('Node Set Name')
 
-# Calculate null AUPRC distribution
+# Calculate robust z-score for each AUPRC (requires network, and SBNE node sub-sampling matrix)
+def calculate_robust_z(network, binary_node_sub_sample_matrix, n_shuffles):
+		# Calculate AUPRC result of node sets on actual network
+	actualnet_AUPRC_result = AUPRC_Analysis(network, binary_node_sub_sample_matrix)['Avg AUPRC']
 
-# Calculate robust z-score for each AUPRC
+	# Calculate AUPRC results of node set on shuffled networks
+	shuffnet_AUPRCs = []
+	for i in range(n_shuffles):
+		# Shuffle network
+		shuffled_network = shuffle_network(network)
+		# Calculate AUPRC Results
+		shuffnet_AUPRCs.append(AUPRC_Analysis(shuffled_network, binary_node_sub_sample_matrix)['Avg AUPRC'])
+	# Concatinate shuffled network AUPRC results together
+	shuffnet_AUPRC_table = pd.concat([pd.Series(shuffnet_AUPRCs, name="Shuffled_Network_"+repr(i+1)) for i in range(n_shuffles)], axis =1)
 
-# SBNE Wrapper function
+	# Calculate robust z-score of network for each node set
+	# Mean absolute deviation scaling factor to make median absolute deviation behave similarly to the standard deviation of a normal distribution
+	k = 1/stats.norm.ppf(0.75)
+	# Compute robust z-score for composite network performances
+	AUPRC_null_median = shuffnet_AUPRC_table.median(axis=1)
+	AUPRC_null_MAD = abs(shuffnet_AUPRC_table.subtract(AUPRC_null_median, axis=0)).median(axis=1)
+	AUPRC_null_MAD_scaled = k*AUPRC_null_MAD
+	AUPRC_ZNorm = (actualnet_AUPRC_result - AUPRC_null_median).divide(AUPRC_null_MAD_scaled)
+	return AUPRC_ZNorm.to_dict()
 
 
 
